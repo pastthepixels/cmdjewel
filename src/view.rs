15de8cb09ba81143273579ba437bbac483407378
PastThePixels <pastthescreen@protonmail.com>
2024-08-12 15:24:12 -0600

    Add animations

--- src/view.rs
+++ src/view.rs
@@ -13,6 +13,17 @@ pub enum CursorMode {
     Swap,
 }
 
+/// Animations
+pub enum AnimationType {
+    Highlight,
+}
+
+pub struct Animation {
+    pub point: game::Point,
+    pub duration: u8,
+    pub animation_type: AnimationType,
+}
+
 impl std::fmt::Display for CursorMode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -25,6 +36,7 @@ impl std::fmt::Display for CursorMode {
 pub struct BoardView {
     board: Board,
     has_focus: bool,
+    animations: Vec<Animation>,
     pub cursor_mode: CursorMode,
 }
 
@@ -33,6 +45,7 @@ impl BoardView {
         BoardView {
             board: Board::new(),
             has_focus: false,
+            animations: Vec::new(),
             cursor_mode: CursorMode::Normal,
         }
     }
@@ -47,6 +60,35 @@ impl BoardView {
             self.board.swap(direction.clone());
         }
         self.cursor_mode = CursorMode::Normal;
+    }
+
+    /// Updatess all animations. Animations are automatically destroyed in the event loop.
+    fn update_animations(&mut self) {
+        // Reduce duration of each animation
+        self.animations.iter_mut().for_each(|animation| {
+            if animation.duration != 0 {
+                animation.duration -= 1;
+            }
+        });
+    }
+
+    /// Creates all animations.
+    fn create_animations(&mut self) {
+        // Highlight all matching gems
+        self.board.get_matching_gems().iter().for_each(|x| {
+            self.animations.push(Animation {
+                point: *x,
+                duration: 8,
+                animation_type: AnimationType::Highlight,
+            });
+        });
+    }
+
+    /// Updates board logic.
+    fn update_board(&mut self) {
+        self.board.update_matching_gems();
+        self.board.fill_from_top();
+        self.board.update_physics_frame();
     }
 }
 
@@ -74,12 +116,25 @@ impl cursive::view::View for BoardView {
                 game::Gems::Orange => (Color::Rgb(207, 135, 111), Color::Rgb(46, 52, 64)),
                 game::Gems::Purple => (Color::Rgb(174, 174, 255), Color::Rgb(46, 52, 64)),
             };
-            if i == self.board.point_to_index(self.board.get_cursor()) {
-                color = match self.cursor_mode {
-                    CursorMode::Normal => (Color::Rgb(46, 52, 64), Color::Rgb(213, 219, 230)),
-                    CursorMode::Swap => (Color::Rgb(213, 219, 230), Color::Rgb(190, 96, 105)),
+            // Swap colors for highlighted gems.
+            self.animations.iter().for_each(|anim| {
+                if anim.point.0 == point.0 && anim.point.1 == point.1 {
+                    let foreground = color.0;
+                    color.0 = color.1;
+                    color.1 = foreground;
+                }
+            });
+            // If there's no animation happening, you can theme the cell under whatever conditions.
+            if self.animations.len() == 0 {
+                // for instance, this is the cursor.
+                if i == self.board.point_to_index(self.board.get_cursor()) {
+                    color = match self.cursor_mode {
+                        CursorMode::Normal => (Color::Rgb(46, 52, 64), Color::Rgb(213, 219, 230)),
+                        CursorMode::Swap => (Color::Rgb(213, 219, 230), Color::Rgb(190, 96, 105)),
+                    }
                 }
             }
+            // If the board is not focused, grey out everything.
             if !self.has_focus {
                 color = (Color::Rgb(76, 86, 106), Color::Rgb(59, 66, 82))
             }
@@ -90,14 +145,35 @@ impl cursive::view::View for BoardView {
     }
 
     fn take_focus(&mut self, _: Direction) -> Result<EventResult, CannotFocus> {
+        self.has_focus = true;
         Ok(EventResult::Consumed(None))
     }
 
     fn on_event(&mut self, event: Event) -> EventResult {
-        self.has_focus = true;
-        self.board.update_matching_gems();
-        self.board.fill_from_top();
-        self.board.update_physics_frame();
+        // Handles losing focus
+        if let Event::FocusLost = event {
+            self.has_focus = false;
+            return EventResult::Ignored;
+        }
+        // Updates animations
+        self.update_animations();
+        let mut is_animation_removed = false;
+        for i in (0..self.animations.len()).rev() {
+            if self.animations[i].duration == 0 {
+                self.animations.remove(i);
+                is_animation_removed = true;
+            } else {
+                return EventResult::Ignored;
+            }
+        }
+        if !is_animation_removed {
+            self.create_animations();
+        }
+        // Update board
+        if self.animations.len() == 0 {
+            self.update_board();
+        }
+        // Handle events
         match event {
             Event::Char(c) => match c.to_ascii_lowercase() {
                 ' ' => {
@@ -108,11 +184,9 @@ impl cursive::view::View for BoardView {
                     }
                     EventResult::consumed()
                 }
-                _ => {
-                    return EventResult::with_cb(move |s| {
-                        s.add_layer(Dialog::info("Key not recognized. Use the arrow keys to move and the enter key to enter SWAP mode."));
-                    })
-                }
+                _ => EventResult::with_cb(move |s| {
+                    s.add_layer(Dialog::info("Key not recognized. Use the arrow keys to move and the enter key to enter SWAP mode."));
+                }),
             },
             Event::Key(cursive::event::Key::Left) => match self.cursor_mode {
                 CursorMode::Swap => {
@@ -177,10 +251,6 @@ impl cursive::view::View for BoardView {
                     self.cursor_mode = CursorMode::Normal
                 }
                 EventResult::consumed()
-            }
-            Event::FocusLost => {
-                self.has_focus = false;
-                EventResult::Ignored
             }
             _ => EventResult::Ignored,
         }
